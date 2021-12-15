88c1647875f083713906b36be56c25bbea6bc950
SpecU <dawopi2@gmail.com>
2021-12-15 12:52:48 +0100

    bugfix

--- app/Collections/DisorderCollection.php
+++ app/Collections/DisorderCollection.php
@@ -3,37 +3,22 @@
 namespace App\Collections;
 
 use App\Models\Disorder;
-use Illuminate\Database\Eloquent\Collection;
-use App\Repositories\DisordersRepositoryInterface;
 
 class DisorderCollection extends BaseEloquentCollection
 {
-
-
-    public function __construct(array $models)
-    {
-        parent::__construct($models);
-    }
 
 
     /**
      * @param  array  $parameters
      * @return DisorderCollection
      */
-    public function valid(array $parameters) : DisorderCollection
+    public function valid(array $parameters): DisorderCollection
     {
         return $this->filter(fn(Disorder $disorder) => $disorder->valid($parameters));
     }
 
     public function criteria()
     {
-        return $this->pluck('criteria');
+        return $this->pluck('criteria')->first();
     }
-
-
-
-
-
-
-
 }
